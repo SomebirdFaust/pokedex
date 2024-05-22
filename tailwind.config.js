32f98cdc065860ca45c3c2b65281b4d296ecedfd
SomebirdFaust <faust.gaulard@gmail.com>
2024-05-22 09:33:25 +0200

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,vue,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx,vue}',
+    './public/index.html',
+  ],
   theme: {
-    colors : {
-      gray: {
-        100: '#f7fafc'
-      }
-    },
     extend: {},
   },
   plugins: [],
-}
-
-module.exports = {
-  theme: {
-    borderRadius: {
-      'none' : '0',
-      'sm' : '0.125rem',
-      DEFAULT: '.25rem',
-      'lg': '.5rem',
-      'full': '9999px',
-    },
-    extend: {
-      colors: {
-        red: 'rgb(255, 200, 200)',
-        blue: 'rgb(152, 186, 255)',
-      }
-    }
-  }
-}
+};
